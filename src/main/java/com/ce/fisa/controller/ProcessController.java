424a099b2083df6d90d12bd93dcadaab5dbd4ef9
ChoiYoungHa <askillofgod159@naver.com>
2024-10-01 10:39:35 +0900

    test: ci/cd 테스트

--- src/main/java/com/ce/fisa/controller/ProcessController.java
+++ src/main/java/com/ce/fisa/controller/ProcessController.java
@@ -12,6 +12,6 @@ public class ProcessController {
 	@GetMapping("/test")
 	public String reqRes() {
 		log.info("요청 수락 ~~~");
-		return "<h1>linux 서버에서 실행되는 app 입니다</h1>";
+		return "<h1>linux 서버에서 실행되는 app 입니다 cicd 테스트!</h1>";
 	}
 }
